713eaba5ef9e7e7ce22d2d752218a2752de19bc5
sa <pps6777@gmail.com>
2017-03-07 18:30:09 +0900

    seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+blogtitles = [
+    'テストです',
+    '3月',
+    '7月',
+    '1月'
+]
+
+blogcontents = [
+    '今日は良い日',
+    '桜が咲いた',
+    '暑かった',
+    'お正月'
+]
+
+poemtitles = [
+    '花が',
+    'チューリップ',
+    'みんな',
+    '4月'
+]
+
+poemcontents = [
+    '咲いた',
+    '赤白黄',
+    'きれいだな',
+    '桜'
+]
+
+n=0
+while n <= 3
+    Blog.create!(
+        title: blogtitles[n],
+        content: blogcontents[n]
+    )
+    Poem.create!(
+        title: poemtitles[n],
+        content: poemcontents[n]
+    )
+    n= n+1
+end
